1fcbe8d488adacd7da58f18fba9c052873ef101c
Thanujram <60811416+Thanujram@users.noreply.github.com>
2022-10-24 21:07:40 +0530

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 6, name:'Le-bron James' },
+	{ id: 23, name:'Antony Davis' },
+	{ id: 34, name:'Giannis Antetokounmpo' },
+	{ id: 0, name:'Damian Lillard' },
+	{ id: 2, name:'Lonzo Ball' },
+	{ id: 1, name:'Zion Willamson' },
+	{ id: 14, name:'Brandon Ingram' }
+];
